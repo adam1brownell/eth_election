d890c2bb71563a13870994fa07f82732295cf2db
adam1brownell <adam1brownell@gmail.com>
2021-04-20 21:45:13 -0700

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+
+  // Store Candidate
+  // Read Candidate
+  string public candidate;
+  // Constructor
+  constructor() public {
+    candidate = "Candidate 1";
+  }
+}
